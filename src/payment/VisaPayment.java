da347f3b3ed5acb0756a59a94293d4b5364b4ead
Tynchtyk <baizakovtm@gmail.com>
2024-01-24 23:07:54 +0600

    scrum-162/Feature-Visa Payment

---
+++ src/payment/VisaPayment.java
@@ -0,0 +1,7 @@
+package payment;
+
+public class VisaPayment {
+    public void payByVisa (String cardholderName, int cardNumber) {
+        System.out.println(cardholderName+ " " + cardNumber);
+    }
+}
